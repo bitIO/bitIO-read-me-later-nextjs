f093fd52d9a4a36c7c27a2944c058b96b394c330
Francisco Calle Moreno <francisco.calle.moreno@gmail.com>
2022-07-28 21:04:45 +0200

    chore: setup eslint import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,19 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: ['builtin', 'external', 'internal', 'sibling', 'parent', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [{ pattern: 'react', group: 'builtin', position: 'before' }],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
     'react/jsx-sort-props': 'error',
     'react/react-in-jsx-scope': 'off',
     'sort-keys': ['error', 'asc', { caseSensitive: true, natural: false, minKeys: 2 }],
