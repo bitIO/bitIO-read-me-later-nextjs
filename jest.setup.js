fd1e8efdc2b26b053877fb556e163322fb5b2f53
Francisco Calle Moreno <francisco.calle.moreno@gmail.com>
2022-07-28 10:49:08 +0200

    test: add auth0 global mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('@auth0/nextjs-auth0', () => ({
+  useUser: jest.fn().mockImplementation(() => ({
+    isAuthenticated: true,
+    loginWithRedirect: jest.fn(),
+    logout: jest.fn(),
+    user: {
+      email: 'jest@google.com',
+      email_verified: true,
+      sub: 'google-oauth2|12345678901234',
+    },
+  })),
+  withAuthenticationRequired: jest.fn().mockImplementation((component, ignore) => component),
+}));
